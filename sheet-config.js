bd3cbe38ea96effaa572e74b52124c1b710146a1
shabydevelop-code <shaby.develop@gmail.com>
2026-03-26 00:26:56 +0200

    Delete sheet-config.js

--- sheet-config.js
+++
@@ -1,3 +0,0 @@
-window.LIHI_SHEET_CONFIG = {
-  csvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSBGXmDJTtvXvaWIvKsfan_bfD1szA5ZFUtupKjgdGTFsL7GJiBm7giLvT8bm2A6v54Ev3AfrnCAEL7/pub?output=csv",
-};
